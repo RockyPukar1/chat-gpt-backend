bb76dae2836efbb3acecb9afda6eff2a1b049c4c
Pukar Chhatkuli <22.student.pukar@broadwayinfosys.edu.np>
2023-03-17 19:16:31 +0545

    1:07:44

--- server.js
+++ server.js
@@ -9,26 +9,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async(req, res) => {
-    res.status(200).json({
-        message: "This is ChatGPT AI App"
-    });
-})
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 app.post("/", async (req, res) => {
-    try {
-        
-    } catch(err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-})
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: req.body.input,
+      temperature: 0,
+      max_tokens: 4000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0
+    });
+    console.log("PASSED: " , req.body.input);
+    res.status(200).send({
+        bot: response.data.choices[0].text
+    })
+} catch (err) {
+    console.log("FAILED: " , req.body.input);
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-app.listen('4000', "localhost", (err) => {
-    if (err) {
-        console.log("Error Listening to PORT 4000: ", err);
-    } else {
-        console.log("Server is listening at PORT 4000");
-        console.log("Press CTRL+C to end the server");
-    }
-})
+app.listen("4000", "localhost", (err) => {
+  if (err) {
+    console.log("Error Listening to PORT 4000: ", err);
+  } else {
+    console.log("Server is listening at PORT 4000");
+    console.log("Press CTRL+C to end the server");
+  }
+});
